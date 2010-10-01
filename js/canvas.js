9f6f14ab5e4b2b1a4b1acb8a9703260e61738eb2
Dexter <poyzer@gmail.com>
2010-10-01 17:59:05 -0400

    don't know what I changed, added processing.js though, so that's good

--- js/canvas.js
+++ js/canvas.js
@@ -4,10 +4,8 @@ $(document).ready(function() {
 	if (canvas.getContext) {  
         var ctx = canvas.getContext("2d");  
   
-        ctx.fillStyle = "rgb(200,0,0)";  
-        ctx.fillRect (10, 10, 55, 50);  
-  
-        ctx.fillStyle = "rgba(0, 0, 200, 0.5)";  
-        ctx.fillRect (30, 30, 55, 50);  
-      }
+	    ctx.fillRect(25,25,100,100);  
+        ctx.clearRect(45,45,60,60);  
+        ctx.strokeRect(50,50,50,50);  
+    }
 });
